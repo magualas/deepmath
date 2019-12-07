fc6bb144d41422fe1c5fffa43ff1ac8241984fea
EC2 Default User <ec2-user@ip-172-16-67-231.us-east-2.compute.internal>
2019-12-07 18:48:50 +0000

    compute

--- deepmath/deephol/train/extraction_pipeline.py
+++ deepmath/deephol/train/extraction_pipeline.py
@@ -12,11 +12,11 @@ import tensorflow as tf
 import tensorflow.contrib.eager as tfe
 import boto3
 # print(tf.__version__)
-
 import ingestor
 import extractor2
 import data
 import functools
+import progressbar
 
 
 # config
@@ -26,7 +26,6 @@ paths = {
     'valid':'',
     'test':''
 }
-
 
 def run_extraction_pipeline():
     # get dataset parameters
@@ -42,38 +41,30 @@ def run_extraction_pipeline():
     labels = {'tac_id': []}
 
     # iterate over dataset to extract data into arrays. 
-    # remove 'take' part to iterate over the entire dataset
-    train_parsed = train_parsed.take(2000)
-    for raw_record in train_parsed:
+    maxval_bar = 10000
+    pbar = progressbar.ProgressBar(maxval=maxval_bar)
+    train_parsed = train_parsed.take(10000)  # remove 'take' part to iterate over the entire dataset
+    for raw_record in pbar(train_parsed):
         fx, lx = raw_record[0], raw_record[1]
         features['goal'].append(fx['goal'])
         features['goal_asl'].append(fx['goal_asl'])
         features['thms'].append(fx['thms'])
         features['thms_hard_negatives'].append(fx['thms_hard_negatives'])
         labels['tac_id'].append(lx['tac_id'])
-    print('Done extracting data into arrays for goals, thms and tactics')
+    print('Success: extracting data into arrays for goals, thms and tactics')
 
     # instantiate extractor object
     ex = extractor2.Extractor(params)
 
     # tokenize goals
-    temp = ex.tokenize(features['goal'], ex.vocab_table)
-    
-    # pad all goals to be of length 1000
-    goal_list = []
-    for j in range(len(temp)):
-            l = len(temp[j])
-            h = tf.pad(temp[j], [[0, 1000-l]], constant_values=0)
-            goal_list.append(h)
-    features['goal_ids'] = goal_list
-    print('Done tokenizing goals')
+    features['goal_ids'] = ex.tokenize(features['goal'], ex.vocab_table)
+    print('Success: tokenizing goals')
 
     # tokenize hypotheses. this requires more work since there may be more than one hypothesis
     length = len(features['goal'])
     features['goal_asl_ids'] = []
     for i in range(length):
         temp = ex.tokenize(features['goal_asl'][i], ex.vocab_table)
-        
         # pad all hypotheses to be of length 1000
         hypo_list = []
         for j in range(len(temp)):
@@ -81,7 +72,7 @@ def run_extraction_pipeline():
             h = tf.pad(temp[j], [[0, 1000-l]], constant_values=0)
             hypo_list.append(h)
         features['goal_asl_ids'].append(hypo_list)
-    print('Done tokenizing hypotheses')
+    print('Success: tokenizing hypotheses')
 
     # free up memory
     del features['goal']
@@ -94,19 +85,21 @@ def run_extraction_pipeline():
     goals_array = np.array(features['goal_ids'])
     upload_np_to_s3(goals_array, os.path.join(paths['train'], 'goal_ids.csv'))
     del goals_array
+    print('Uploaded goals successfully')
     
     # features: hypotheses
-    for i, hyp in enumerate(features['goal_asl_ids']):
+    pbar = progressbar.ProgressBar(maxval=maxval_bar)
+    for i, hyp in pbar(enumerate(features['goal_asl_ids'])):
         upload_np_to_s3(np.array(hyp), 
                         os.path.join(paths['train'], 'goal_asl_ids_{}.csv'.format(i)))
     del features['goal_asl_ids']
+    print('Uploaded hypotheses successfully')
     
     # labels: tactid ids
     labels_array = np.array(labels['tac_id'])
     upload_np_to_s3(labels_array, os.path.join(paths['train'], 'tac_id.csv'))
     del labels_array
-    
-    print('Successfully uploaded to s3 the goals, hypotheses and labels')
+    print('Uploaded labels (tactics) successfully')
     
 
 def upload_np_to_s3(array, object_name):    
