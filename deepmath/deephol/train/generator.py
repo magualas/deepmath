dcbf913726152c652b80e8153d82e0288022663c
EC2 Default User <ec2-user@ip-172-16-93-54.us-east-2.compute.internal>
2019-12-12 06:13:31 +0000

    gen

--- deepmath/deephol/train/generator.py
+++ deepmath/deephol/train/generator.py
@@ -34,19 +34,22 @@ config = {
 }
 os.environ.update(config)
 
+line_counts = {
+    'train':376968,
+    'test':122928,
+    'valid':104054
+}
+
 
 class My_DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, labels, data_set='train',batch_size=64,
+    def __init__(self, data_set='train',batch_size=64,
                  w_hyp=False, n_channels=1, n_classes=41, shuffle=True):
         self.dim = 3000 if w_hyp else 1000 
         self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
-        self.on_epoch_end()
         self.w_hyp = w_hyp
         self.data_set = data_set
         
@@ -54,54 +57,43 @@ class My_DataGenerator(keras.utils.Sequence):
         X_paths, Y_path = self.get_partition_and_labels()
         self.features_keys_lst = X_paths
         self.label_key = Y_path[0]
-#         self.n = self._get_n_rows_parition(self.features_keys_lst[0])
+        self.n = line_counts[self.data_set]
+        self.partition_index = 0
         
         # readers
-        self._initialize_readers()        
+        self._initialize_readers()  
+        
+        self.on_epoch_end()
 
     def __len__(self):
         'Denotes the number of batches per epoch'
         return int(np.floor(self.n / self.batch_size))
 
-    def __getitem__(self, id_lst=[]):
+    def __getitem__(self, index):
         'Generate one batch of data'
-        X, y = next(self.reader_X), next(self.reader_Y)
+        try:
+            X, y = next(self.reader_X[self.partition_index]), next(self.reader_Y)
+        except Exception as e:
+            self.partition_index += 1
+        
 
         return X.values, y.values
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
+        self.list_partitions = self.features_keys_lst
         if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def _get_n_rows_parition(self, key):
-        s3_client = boto3.client('s3') 
-        req = s3_client.select_object_content(
-             Bucket=BUCKET_NAME,
-             Key=key,
-             ExpressionType='SQL',
-             Expression='SELECT COUNT(*) FROM s3object',
-             InputSerialization = {'CSV': {'FileHeaderInfo': 'Use'}},
-             OutputSerialization = {'CSV': {}},
-        )
-        mess = next(req['Payload']._event_generator)
-        print(mess.payload)
-        print(mess)
-        
-        return int(mess.payload)
+            np.random.shuffle(self.list_partitions)
+            
+        self.partition_index =0
     
     def _initialize_readers(self):
-        # must change to all features
-#         obj_X = s3_client.get_object(Bucket=BUCKET_NAME, Key=self.features_keys_lst[0]) 
-        path_X = os.path.join('s3://', BUCKET_NAME, self.features_keys_lst[0])
-        self.reader_X = pd.read_csv(path_X, chunksize=self.batch_size, 
-                                    header=None, engine='python')
+        paths_X = [os.path.join('s3://', BUCKET_NAME, x) for x in self.features_keys_lst]
+        self.readers_X = [pd.read_csv(path, chunksize=self.batch_size, header=None, engine='python')
+                          for path in paths_X]
         
         path_Y = os.path.join('s3://', BUCKET_NAME, self.label_key)
-#         obj_Y = s3_client.get_object(Bucket=BUCKET_NAME, Key=self.label_key)
-        self.reader_Y = pd.read_csv(path_Y, chunksize=self.batch_size,
-                                    header=None, engine='python')
+        self.reader_Y = pd.read_csv(path_Y, chunksize=self.batch_size, header=None, engine='python')
         
         return None
     
@@ -128,3 +120,23 @@ class My_DataGenerator(keras.utils.Sequence):
             X_files = [x for x in partition[self.data_set] if x.find('X_train_hyp') == (-1) and x.find('Y_train') == -1]
 
         return sorted(X_files, key=lambda x: len(x)), y_file
+
+    
+    
+    
+    
+#     def _get_n_rows_parition(self, key):
+#         s3_client = boto3.client('s3') 
+#         req = s3_client.select_object_content(
+#              Bucket=BUCKET_NAME,
+#              Key=key,
+#              ExpressionType='SQL',
+#              Expression='SELECT COUNT(*) FROM s3object',
+#              InputSerialization = {'CSV': {'FileHeaderInfo': 'Use'}},
+#              OutputSerialization = {'CSV': {}},
+#         )
+#         mess = next(req['Payload']._event_generator)
+#         print(mess.payload)
+#         print(mess)
+        
+#         return int(mess.payload)
